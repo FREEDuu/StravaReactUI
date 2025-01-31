c3bc00ea6cbf63f034220b3e0aabdebbee9ac6f7
FREEDuu <derosafrancesco429@gmail.com>
2025-01-31 15:46:33 +0100

    Project setup + tailwindcss setup + login page

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # StravaReactUI
 React repo to display frontend of my strava app
+
+## Project made with React/vite/tailwindcss
---
+++ react-ui/src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+
+      </div>
+      <h1>Benvenuto su Run-up</h1>
+      <h2>Accedi con un click, con Strava</h2>
+      <div className="card">
+      <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 border border-orange-700 rounded">
+        Login con Strava
+      </button>
+
+      </div>
+    </>
+  )
+}
+
+export default App
